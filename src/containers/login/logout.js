eb6a91e6c97bfb16e59201786befd87b60c5f78f
iPavliv <iryna.pavliv96@gmail.com>
2019-07-29 14:24:43 +0300

    add logout.js

---
+++ src/containers/login/logout.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import cookie from 'react-cookies';
+
+class Logout extends Component {
+
+    handleSubmit = event => {
+        const url = "http://127.0.0.1:5050/users/logout";
+
+        axios.post(url,  { withCredentials:true }
+        ).then(response => {
+            cookie.remove('session', { path: '/' });
+            alert('isLoggedIn: ' + response.data['isLoggedIn']);
+        });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Logout">
+                <Form onSubmit={this.handleSubmit}>
+                    <Button
+                        block
+                        type="submit"
+                    >
+                        Logout
+                    </Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default Logout;
