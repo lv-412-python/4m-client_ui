7a9ac14c4344548c52b9d57a27c2d954ed59fd8e
olyapetryshyn <olya.petryshyn@gmail.com>
2019-08-05 17:58:36 +0300

    added ability to edit forms.

---
+++ src/components/forms/formEdit/formEdit.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+import './formEdit.css';
+
+class FormEdit extends Component {
+    state = {
+        // eslint-disable-next-line react/prop-types
+        form_id: this.props.form_id,
+        // eslint-disable-next-line react/prop-types
+        owner: this.props.owner,
+        title: undefined,
+        description: undefined
+    };
+
+    componentDidMount() {
+        axios.get(`http://127.0.0.1/form/${this.state.form_id}`).then(response => {
+            const form = response.data;
+            let object = {
+                description: form.description,
+                title: form.title
+            };
+            this.setState(object);
+            // let value = this.props.selectedItems.map(value => value.id);
+            // object.fields = value;
+        });
+    }
+
+    handleChangeInput = e => {
+        const {name, value} = e.target;
+        this.setState({[name]: value});
+    };
+
+    handleSubmit = (e) => {
+        // eslint-disable-next-line react/prop-types
+        let value = this.props.selectedItems.map(value => value.id);
+        const data = this.state;
+        data.fields = value;
+        e.preventDefault();
+        axios.put(`http://127.0.0.1/form/${this.state.form_id}`, this.state).then(() => {
+            window.location.reload();
+        }).// eslint-disable-next-line no-console
+        catch(error => {
+            // eslint-disable-next-line no-console
+            console.log(error);
+        });
+    };
+
+    render() {
+        const {title, description} = this.state;
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} className='edit_form'>
+                    <label>
+                        <p>Title:</p>
+                        <input type='text' value={title} name='title' className='form_input'
+                               onChange={this.handleChangeInput} placeholder={title}/>
+                    </label>
+                    <label>
+                        <p>Description:</p>
+                        <textarea value={description} name='description' className='form_input'
+                                  onChange={this.handleChangeInput} placeholder={description}/>
+                    </label>
+                    <label>
+                        <p>Fields:</p>
+                    </label>
+                </form>
+                <div className='submit'>
+                    <input className='btn btn-dark submit_btn' type='button' value="Submit"
+                           onClick={this.handleSubmit}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FormEdit;
