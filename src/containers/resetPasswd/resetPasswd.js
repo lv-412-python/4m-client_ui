7a80ffbb8a6c327590940b611016ff4ee1131a00
iPavliv <iryna.pavliv96@gmail.com>
2019-08-07 16:53:04 +0300

    added password reset

---
+++ src/containers/resetPasswd/resetPasswd.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class ResetPassword extends Component {
+    state = {
+      'email': "",
+      'emailErr': ""
+    };
+
+    validateForm() {
+        return this.state['email'].length > 0;
+    }
+
+    validate = () =>  {
+        let emailErr = "";
+
+        if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+            emailErr = "invalid email";
+        }
+        if (emailErr) {
+            this.setState( {emailErr} );
+            return false;
+        }
+
+        return true;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = () => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid) {
+            const emailData = {
+                "email": this.state.email
+            };
+
+            const url = "http://127.0.0.1/reset_password";
+
+            axios.post(url, emailData, { withCredentials:true, crossDomain: true }
+            ).then( response => {
+                alert(response.data.message);
+                window.location = "http://127.0.0.1:3000/set_new_password";
+            }).catch( error => {
+                alert(error.response.data.error);
+            });
+
+            this.setState({
+                emailErr: ""
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="ResetPassword">
+                <div className="email">
+                    <label>Enter your email:</label><br />
+                    <input id="email"
+                        type="email"
+                        value={this.state['email']}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="errorMsg">{this.state.emailErr}</div>
+                <input
+                    disabled={!this.validateForm()}
+                    type="button"
+                    onClick={this.handleSubmit}
+                    value='Send reset request'
+                />
+            </div>
+        );
+    }
+}
+
+export default ResetPassword;
---
+++ src/containers/resetPasswd/setNewPasswd.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import queryString from 'query-string';
+
+class SetNewPassword extends Component {
+    state = {
+      'password': "",
+      'passwordTwo': "",
+      'passwordErr': "",
+      'passwordTwoErr': ""
+    };
+
+    validateForm() {
+        return this.state['password'].length > 0 && this.state['passwordTwo'].length > 0;
+    }
+
+    validate = () => {
+        let passwordErr = "";
+        let passwordTwoErr = "";
+
+        if (this.state.password.length < 6) {
+            passwordErr = "password is too short";
+        }
+        if (this.state.password != this.state.passwordTwo) {
+            passwordTwoErr = "repeat password correctly";
+        }
+        if (passwordErr ||passwordTwoErr) {
+            this.setState( {passwordErr, passwordTwoErr} );
+            return false;
+        }
+
+        return true;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = () => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid) {
+            const newPassword = {
+                "password": this.state.password
+            };
+            // eslint-disable-next-line react/prop-types
+            const values = queryString.parse(this.props.location.search);
+            const url = `http://127.0.0.1/reset_password?token=${values.token}`;
+            axios.put(url, newPassword, { withCredentials:true, crossDomain: true }
+            ).then( response => {
+                alert(response.data.message);
+                window.location = "http://127.0.0.1:3000/login";
+            }).catch( error => {
+                alert(error.response.data.error);
+            });
+
+            this.setState({
+                passwordErr: "",
+                passwordTwoErr: "",
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="ResetPassword">
+                <div className="password">
+                    <label>Password</label><br />
+                    <input id="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                    <div className="errorMsg">{this.state.passwordErr}</div>
+                </div>
+                <div className="password">
+                    <label>Submit password</label><br />
+                    <input id="passwordTwo"
+                        value={this.state.passwordTwo}
+                        onChange={this.handleChange}
+                        type="password"
+                    />
+                    <div className="errorMsg">{this.state.passwordTwoErr}</div>
+                    <input
+                        disabled={!this.validateForm()}
+                        type="button"
+                        onClick={this.handleSubmit}
+                        value='Set new password'
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SetNewPassword;
